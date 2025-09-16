9bb874efbaee21ebd0f1f9d563cb760f8d893b89
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-15 21:21:19 -0300

    exclui tudo

--- exercicios/meus-exercicios/scripts/exercicio2.js
+++
@@ -1,6 +0,0 @@
-    let centenas = prompt('Digite um número com 3 casas decimais');
-        if(centenas>=532 && centenas<=743) {
-            console.log('O número está entre 532 e 743');
-        }
-
-    
