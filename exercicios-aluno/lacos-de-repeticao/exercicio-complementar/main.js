ce8e0343275b7b2315561db946e1b95c0497c901
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-17 19:33:09 -0300

    Mudança de arquivos para a pasta exercicios-aluno

---
+++ exercicios-aluno/lacos-de-repeticao/exercicio-complementar/main.js
@@ -0,0 +1,12 @@
+let n = parseInt(prompt('Insira um número maior do que 1'));
+let soma = 0;
+let contador = 0;
+
+
+while (contador <= n) {
+soma = soma + contador;
+contador = contador + 1; 
+
+}
+
+console.log("A soma é:", soma);
