cb7ae1e5cde2eae6f1be78135efb601dc63309be
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-18 16:10:16 -0300

    Tentativa de execução do exercício

--- exercicios-aluno/lacos-de-repeticao-do-while/exercicio-3/main.js
+++ exercicios-aluno/lacos-de-repeticao-do-while/exercicio-3/main.js
@@ -1,5 +1,14 @@
-let a = parseInt(prompt('Insira o primeiro número'));
-let b = parseInt(prompt('Insira o segundo número'));
-let c = parseInt(prompt('Insira o terceiro número'));
-let d = parseInt(prompt('Insira o quarto número'));
-let e = parseInt(prompt('Insira o quinto número'));
+// let a = parseInt(prompt('Insira o primeiro número'));
+// let b = parseInt(prompt('Insira o segundo número'));
+// let c = parseInt(prompt('Insira o terceiro número'));
+// let d = parseInt(prompt('Insira o quarto número'));
+// let e = parseInt(prompt('Insira o quinto número'));
+
+let numero;
+let maior;
+let contador = 1;
+
+maior = parseInt(prompt("Digite o primeiro número:"));
+
+
+
