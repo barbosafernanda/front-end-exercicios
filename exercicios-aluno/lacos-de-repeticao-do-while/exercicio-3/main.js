78826587543bdfc60186e005488483481158c3c9
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-19 19:20:44 -0300

    Atualização do exercício 3 - Do...While

--- exercicios-aluno/lacos-de-repeticao-do-while/exercicio-3/main.js
+++ exercicios-aluno/lacos-de-repeticao-do-while/exercicio-3/main.js
@@ -1,9 +1,3 @@
-// let a = parseInt(prompt('Insira o primeiro número'));
-// let b = parseInt(prompt('Insira o segundo número'));
-// let c = parseInt(prompt('Insira o terceiro número'));
-// let d = parseInt(prompt('Insira o quarto número'));
-// let e = parseInt(prompt('Insira o quinto número'));
-
 let numero;
 let maior;
 let contador = 1;
