4eac5908eda2f3b41c45428594026da8dd42cf76
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-23 20:07:12 -0300

    Atualização das pastas de exercicio

---
+++ exercicios-aluno/funcoes/funcoes-exercicio-5/main.js
@@ -0,0 +1,17 @@
+// exercicio 4
+
+const mais = function (f, b) {
+    let resultado = f + b;
+    return resultado;
+};
+
+console.log (mais(3, 4));
+console.log (mais(5, 6));
+
+const menos = function (g, h) {
+    let subtracao = g - h;
+    return subtracao; 
+};
+
+console.log 
+
