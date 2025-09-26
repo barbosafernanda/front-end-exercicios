31021d39d650b8da1e773f08e309ef1c9cd69ebb
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-26 20:10:18 -0300

    Resolução dos ecercícios de DOM
    Nâo consegui acompanhar tudo na aula. Revisar resoluções do professor

--- exercicios-aluno/DOM/js/main.js
+++ exercicios-aluno/DOM/js/main.js
@@ -1,10 +1,30 @@
-// let ocortico = document.getElementById('o-cortico');
-// console.log (ocortico);
+let ocortico = document.getElementById('o-cortico');
+console.log (ocortico);
 
-// let anchor = document.getElementsByTagName('a')
-// console.log (anchor);
+let anchor = document.getElementsByTagName('a')
+console.log (anchor);
 
-// let vidasSecas = document.querySelector('#vidas-secas')
-// console.log (vidasSecas);
+let vidasSecas = document.querySelector('#vidas-secas')
+console.log (vidasSecas);
 
+
+let navListFooter = document.querySelector('.footer-links .nav-list')
+console.log (navListFooter);
+
+
+let navListHeader = document.querySelector('.header .nav-list')
+console.log (navListHeader);
+
+// Modificando Elementos
+ocortico.style.color = 'red';
+
+let paragrafo = ocortico.querySelector('p');
+paragrafo.style.color ='red';
+
+// Modificando classes
+console.log ('vidasSecas.className', vidasSecas.className)
+
+vidasSecas.className = 'card-livro fora-de-estoque'
+
+// Modificando imagem de livro 
 
