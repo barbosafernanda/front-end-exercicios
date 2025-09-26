18c8e3dd4e9413ff5d974bfd7be92e022ca39769
barbosafernanda <fervieirabarbosa@gmail.com>
2025-09-25 21:47:01 -0300

    Exercicio de DOM

---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,10 @@
+// let ocortico = document.getElementById('o-cortico');
+// console.log (ocortico);
+
+// let anchor = document.getElementsByTagName('a')
+// console.log (anchor);
+
+// let vidasSecas = document.querySelector('#vidas-secas')
+// console.log (vidasSecas);
+
+
